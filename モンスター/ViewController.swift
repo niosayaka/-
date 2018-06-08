c4d7b6a6d63f2167cd45956095e284c890d6b4e1
niosayaka <niosayaka@gmail.com>
2018-06-08 23:43:01 +0900

    commit

--- モンスター/ViewController.swift
+++ モンスター/ViewController.swift
@@ -9,11 +9,69 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageview:UIImageView!
+    @IBOutlet var label:UILabel!
+    
+    var dispImageNo = 0
+    
+    @IBAction func next(_sender: AnyObject){
+        
+        dispImageNo += 1
+        
+        displayImage()
+        
+    }
+    
+    @IBAction func back(_sender: AnyObject){
+        
+        dispImageNo -= 1
+        
+        displayImage()
+        
+    }
+    
+    func displayImage() {
+        
+        let imageNameArray = [
+        "image1.jpg",
+        "image2.jpg",
+        "image3.jpg",
+        ]
+        
+        if dispImageNo < 0 {
+            dispImageNo = 2
+            
+        }
+        
+        if dispImageNo > 2 {
+            dispImageNo = 0
+        }
+        
+        let name = imageNameArray[dispImageNo]
+        
+        let image = UIImage(named: name)
+        if dispImageNo == 0 {
+            label.text = "黒沢さん"
+            
+        }else if dispImageNo == 1 {
+            label.text = "三沢さん"
+            
+        }else if dispImageNo == 2 {
+            label.text = "大沢さん"
+        }
+        
+        imageview.image = image
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let image = UIImage(named: "image1")
+        imageview.image = image
     }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
