b235733849e66ab0345f627fa441df4caf3d138d
niosayaka <niosayaka@gmail.com>
2018-06-15 18:36:39 +0900

    commit

--- モンスター/ViewController.swift
+++ モンスター/ViewController.swift
@@ -14,22 +14,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var label: UILabel!
     
     var monster: [Monster] = []
-    var number : Int = 0
+    var number: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let monster1 = Monster()
         monster1.image = UIImage(named: "image1.jpg")
-        monster1.name = "黒沢さん"
+        monster1.name = "bird1"
         
         let monster2 = Monster()
         monster2.image = UIImage(named: "image2.jpg")
-        monster2.name = "三沢さん"
+        monster2.name = "bird2"
         
         let monster3 = Monster()
         monster3.image = UIImage(named: "image3.jpg")
-        monster3.name = "大沢さん"
+        monster3.name = "bird3"
         
         monster = [monster1, monster2, monster3]
         
@@ -45,8 +45,8 @@ class ViewController: UIViewController {
         if number > 2 {
             number = 0
         }
-        label.text = monster[number].name
-        imageView.image = monster[number].image
+        label.text = monster[index].name
+        imageView.image = monster[index].image
     }
     
     @IBAction func back(_ sender: Any) {
@@ -54,7 +54,16 @@ class ViewController: UIViewController {
         if number < 0 {
             number = 2
         }
-        label.text = monster[number].name
-        imageView.image = monster[number].image
+        label.text = monster[index].name
+        imageView.image = monster[index].image
         
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
 }
+
